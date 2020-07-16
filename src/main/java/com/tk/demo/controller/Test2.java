d067b9ce541011f91adccbb2d3958f5ee9e8cec4
weiyf16 <1874152584@qq.com>
2020-07-16 16:24:45 +0800

    更新 Test2

--- src/main/java/com/tk/demo/controller/Test2.java
+++ src/main/java/com/tk/demo/controller/Test2.java
@@ -3,6 +3,7 @@ package com.tk.demo.controller;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("-------------------------");
+        System.out.println("hello world");
 //       throw new RuntimeException("sssss");
 
     }
