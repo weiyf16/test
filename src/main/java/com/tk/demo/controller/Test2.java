96538a2bfed56b2e4394bb06980ebf5135a1a832
weiyf16 <1874152584@qq.com>
2020-07-16 22:06:30 +0800

    添加 20200716

--- src/main/java/com/tk/demo/controller/Test2.java
+++ src/main/java/com/tk/demo/controller/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("-------------------------");
         System.out.println("hello world");
+        System.out.println("20200716");
 //       throw new RuntimeException("sssss");
 
     }
