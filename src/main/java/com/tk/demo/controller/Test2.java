db2ae05b1f22c35af10b63582a577fed6e2ef51d
weiyf16 <1874152584@qq.com>
2020-07-16 15:52:43 +0800

    修改文件

--- src/main/java/com/tk/demo/controller/Test2.java
+++ src/main/java/com/tk/demo/controller/Test2.java
@@ -3,7 +3,7 @@ package com.tk.demo.controller;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("-------------------------");
-       throw new RuntimeException("sssss");
+//       throw new RuntimeException("sssss");
 
     }
 }
